0bc5d5586ff7c1155d784b2784dd3f0c46ce5e46
sharmaine345 <sharmaineannemoreno7@gmail.com>
2024-09-23 10:13:19 +0800

    Activity 1.js

---
+++ Activity 1.js
@@ -0,0 +1,12 @@
+// Create variable name
+
+let name = "sharmaine Moreno";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Female";
+let facebookAcct = "Sharmaine Moreno";
+
+// Log all variables to the console
+console.log("Name:", name);
+console.log("What do you wanna become in your life:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Facebook Account:", facebookAcct);
