b004725481295a5f8f9eca9936151fc2986075a0
sharmaine345 <sharmaineannemoreno7@gmail.com>
2024-09-23 10:53:30 +0800

    Activity 3.js

---
+++ Activity 3.js
@@ -0,0 +1,10 @@
+//booleans 
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 5;  
+let result = favNumb + undefined;
+
+console.log('isJsProgrammingLanguage:', isJsProgrammingLanguage);
+console.log('isJsHard:', isJsHard);
+console.log('favNumb:', favNumb);
+console.log('Result of favNumb + undefined:', result);
