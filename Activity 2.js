1ed387e34205f6e5c6a5d8e51b20fe4b4e0c90e8
sharmaine345 <sharmaineannemoreno7@gmail.com>
2024-09-23 10:44:33 +0800

    Activity 2.js

---
+++ Activity 2.js
@@ -0,0 +1,17 @@
+let firstFavNum = 5;   
+let secondFavNum = 14;
+let sum = firstFavNum + secondFavNum;
+let difference = firstFavNum - secondFavNum;
+let product = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let remainder = firstFavNum % secondFavNum;
+let power = Math.powfirstFavNum, secondFavNum;
+
+console.log(" FavNumber:", firstFavNum);
+console.log(" FavNumber:", secondFavNum);
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Division:", division);
+console.log("Remainder (Modulus):", remainder);
+console.log("Power:", power);
