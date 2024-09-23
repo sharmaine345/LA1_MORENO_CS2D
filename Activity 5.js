aedb6837c507f0c7443e2a8b119caa0f1e9a5843
sharmaine345 <sharmaineannemoreno7@gmail.com>
2024-09-23 11:16:05 +0800

    Activity 5.js

---
+++ Activity 5.js
@@ -0,0 +1,24 @@
+// 1. Create a variable named favActorFirstName & store your favorite actor's first name.
+let favActorFirstName = "Nora";
+
+// 2. Create a variable named favActorLastName & store the last name of your favorite actor.
+let favActorLastName = "Aunor";
+
+// 3. Create a variable named fullName & concatenate favActorFirstName and favActorLastName.
+let fullName = favActorFirstName + " " + favActorLastName;
+
+// 4. Create a variable named uppercase & capitalize your favorite actor's full name.
+let uppercase = fullName.toUpperCase();
+
+// 5. Create a variable named message & store a sentence about your favorite actor with their name in UPPERCASE.
+let message = `My favorite actor is ${uppercase}. . She is best known for her powerful performances in films and her contributions to the country's entertainment industry.`;
+
+// 6. Append the sentence "his best show is Silicon Valley" to the message variable.
+message += " Her best show is himala.";
+
+// 7. Log all variables to the console.
+console.log("First Name:", favActorFirstName);
+console.log("Last Name:", favActorLastName);
+console.log("Full Name:", fullName);
+console.log("Uppercase Name:", uppercase);
+console.log("Message:", message);
